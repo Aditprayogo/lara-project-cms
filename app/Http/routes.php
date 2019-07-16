428bb0c34e5ad7499ca564d7364e57f96eeb0c90
Aditprayogo <adit.ihzar@gmail.com>
2019-07-16 13:21:15 +0700

    adding categories index and able to create categories

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,9 +29,13 @@ Route::group(['middleware' => 'admin'] ,function(){
 
     Route::resource('/admin/posts', 'AdminPostsController');
     
+    Route::resource('/admin/categories', 'AdminCategoriesController');
+    
+    
     Route::get('/admin', function(){
 
         return view('admin.index');
+
     });
 
 });
---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Categories</h1>
+    <?php $i=1; ?>
+    <div class="col-sm-6">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th scope="col">No </th>
+                <th scope="col">Category</th>
+                <th scope="col">Created</th>
+                <th scope="col">Updated</th>
+
+                
+            </tr>
+            </thead>
+            <tbody>
+            
+
+                @if ($categories)
+                    @foreach ($categories as $category)
+                        <tr>
+                            <th scope="row">{{$i++}}</th>
+                            <td>{{$category->name}}</td>
+                            <td>{{$category->created_at->diffForHumans()}}</td>
+                            <td>{{$category->updated_at->diffForHumans()}}</td>
+                        </tr>
+                    @endforeach
+                @endif
+                
+                
+            
+            
+            </tbody>
+        </table>
+    </div>
+    
+@endsection
