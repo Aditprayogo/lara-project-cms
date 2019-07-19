2185192a7f5e4c791965496b71e0bb647d0a86ea
Aditprayogo <adit.ihzar@gmail.com>
2019-07-19 13:35:13 +0700

    displaying user status on dashborad

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,41 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>admin</h1>
+    <h1>Admin</h1>
+    <div class="col-sm-3">
+        @if (Auth::user()->photo)
+            <img src="{{Auth::user()->photo->file}}" alt="" class="img-responsive img-rounded">
+        @else 
+            <img src="/images/noimage.png" alt="" class="img-responsive img-rounded">
+        @endif      
+    </div>
+    <div class="col-sm-9">
+        <div class="well">
+            <ul>
+                <li>
+                    Name : {{Auth::user()->name}}
+                </li>
+                <li>
+                    Role : {{Auth::user()->role->name}}
+                </li>
+                <li>
+                    Created :  {{Auth::user()->created_at->diffForHumans()}}
+                </li>
+                <li>
+                    @if (Auth::user()->is_active == 1)
+
+                        Status : Active 
+             
+                    @else 
+                        Status : Non Active
+             
+                    @endif
+                </li>
+                <li>
+                    Posts :  {{Auth::user()->posts()->count()}}
+                </li>
+            </ul>
+        </div>
+    </div>
+   
 @endsection
