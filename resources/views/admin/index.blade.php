f268ff5abb105ad9a002d9fbc73290301681a32e
Aditprayogo <adit.ihzar@gmail.com>
2019-10-27 00:56:17 +0700

    adding pie chart

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,10 +1,22 @@
 @extends('layouts.admin')
 
 @section('content')
+
+
     
   
+<div class="row">
+    <div class="col-lg-8">
+        <canvas id="myChart"></canvas>
+    </div>
 
-    <canvas id="myChart"></canvas>
+    <div class="col-lg-4">
+        <canvas id="myPieChart"> </canvas>
+    </div>
+</div>
+   
+
+   
    
 @endsection
 
@@ -50,6 +62,46 @@
                 }
             }
         });
+
+        var ctx2 = document.getElementById('myPieChart').getContext('2d');
+        var myPieChart = new Chart(ctx2, {
+            type: 'pie',
+            data: {
+                labels: ['Posts', 'Categories', 'Comments', 'Replies'],
+                datasets: [{
+                    label: 'Data of CMS',
+                    data: [ {{$postsCount}}, {{$categoriesCount}}, {{$commentsCount}}, {{$repliesCount}}],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+
     </script>
     
 @endsection
